9e60cd0471e263b65362163d8eb3486a883be741
Julius <use.julius.gutierrez@gmail.com>
2020-02-17 00:31:30 +0800

    add mobile version

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
+
+  /* For the sticky navigation */
+  $('.hero-text-box').waypoint(function(direction) {
+    if (direction == "down") {
+        $('nav').addClass('sticky');
+    } else {
+        $('nav').removeClass('sticky');
+    }
+  }, {
+    offset: '60px;'
+  });
+
   
   $('.js--wp-2').waypoint(function(direction) {
     $('.js--wp-2').addClass('animated fadeInUp');
@@ -26,7 +38,20 @@ $(document).ready(function() {
   });
 });
 
-
-
+  /* Mobile navigation */
+  $('.js--nav-icon').click(function() {
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
+    
+    nav.slideToggle(200);
+    
+    if (icon.hasClass('ion-navicon-round')) {
+        icon.addClass('ion-close-round');
+        icon.removeClass('ion-navicon-round');
+    } else {
+        icon.addClass('ion-navicon-round');
+        icon.removeClass('ion-close-round');
+    }        
+  });
 
 });
