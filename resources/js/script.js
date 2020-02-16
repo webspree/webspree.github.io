36917a06744a9cba9de7b7485b4625102597ed12
Julius <use.julius.gutierrez@gmail.com>
2020-02-16 22:07:52 +0800

    added css effects

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,15 +1,32 @@
-var $form = $('#contact-form'),
-    url = 'https://script.google.com/macros/s/AKfycbzVv4Dr5Uw2QWZfkFo092okFfm3NUdUw-3FnRww0LvlH7BzNF2V/exec'
+$(document).ready(function() {
+  
+  $('.js--wp-2').waypoint(function(direction) {
+    $('.js--wp-2').addClass('animated fadeInUp');
+  }, {
+    offset: '50%'
+  });
 
-$('#submit-form').on('click', function(e) {
-  e.preventDefault();
-  var jqxhr = $.ajax({
-    url: url,
-    method: "GET",
-    dataType: "json",
-    data: $form.serializeObject()
-  }).success(
-    // do something
-    alert("done")
-  );
-})
+  $('.js--scroll-start').click(function () {
+    $('html, body').animate({scrollTop: $('.js--here-at').offset().top}, 1000); 
+ });
+ 
+ /** nav auto scroll */
+ $(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+
+
+});
